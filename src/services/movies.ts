19347752432230582152ae3dadfcfa36b34b6e54
Ankit Upadhyay <uankitu@gmail.com>
2024-06-14 02:53:13 +0530

    Updated the server in movies service

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -3,6 +3,6 @@ import { Movie } from "../types/movie"
 
 
 export const get = async (): Promise<Movie[]> => {
-    const { data } = await axios.get("http://localhost:3000/movies")
+    const { data } = await axios.get("https://teatime-server-f4587f8da1de.herokuapp.com/movies")
     return data
 }
